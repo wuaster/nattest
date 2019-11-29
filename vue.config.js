4feee75d2a8f822f9baa4b39a4767bed2647e3ec
Faiq Malik <faiqmalik2@gmail.com>
2019-11-29 00:30:44 -0500

    update

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
   assetsDir: "../static",
-  publicPath: "/",
-  outputDir: path.resolve(__dirname, "public/"),
+  publicPath: "/template",
+  outputDir: path.resolve(__dirname, "public/template"),
   runtimeCompiler: undefined,
   productionSourceMap: undefined,
   parallel: undefined,
