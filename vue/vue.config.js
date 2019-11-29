fb6e231455f21d94fbca6a97ec56ca8aabc075d1
wuaster <41026689+wuaster@users.noreply.github.com>
2019-11-28 23:21:14 -0500

    Update vue.config.js

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "vuetify"
   ],
   assetsDir: '../static',
-  //baseUrl: '',
+  baseUrl: '/',
   publicPath: undefined,
   outputDir: path.resolve(__dirname, '../app/templates'),
   runtimeCompiler: undefined,
